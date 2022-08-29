a7333212c1d41c014585258b77026465b23a0b43
Miroslava <miroslava.buchtikova@bluestep.se>
2022-08-29 20:17:20 +0200

    Add client

---
+++ University/University/ClientApp/src/app/models/student.ts
@@ -0,0 +1,13 @@
+export interface StudentDto {
+  ssn: string;
+  name?: string;
+  email?: string;
+  course1?: string;
+  course1Grade?: string;
+  course1DisenrollmentComment?: string;
+  course1Credits?: number;
+  course2?: string;
+  course2Grade?: string;
+  course2DisenrollmentComment?: string;
+  course2Credits?: number;
+}
